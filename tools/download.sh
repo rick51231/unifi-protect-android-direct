afdf6892d92354d2fb9ff466cd67b201c9070c33
rick51231 <rick51231@users.noreply.github.com>
2023-01-04 19:08:20 +0300

    Android signing testkey.

--- tools/download.sh
+++ tools/download.sh
@@ -3,3 +3,5 @@
 wget -O baksmali.jar https://bitbucket.org/JesusFreke/smali/downloads/baksmali-2.5.2.jar
 wget -O apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.6.0.jar
 wget -O sign.jar https://github.com/appium-boneyard/sign/releases/download/1.0/sign-1.0.jar
+wget https://github.com/appium-boneyard/sign/raw/master/testkey.pk8
+wget https://github.com/appium-boneyard/sign/raw/master/testkey.x509.pem
