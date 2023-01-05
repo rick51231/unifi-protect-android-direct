660638238da961d7dfdcc3f25378776a8a155cc7
rick51231 <rick51231@users.noreply.github.com>
2023-01-05 15:27:49 +0300

    Added tools: apksigner, zipalign (linux binary)

--- build_apk.sh
+++ build_apk.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+
 java -jar tools/apktool.jar b -o protect-patched-unsigned.apk out
-zipalign -p 4 protect-patched-unsigned.apk protect-patched-unsigned-aligned.apk
-apksigner sign --key tools/testkey.pk8 --cert tools/testkey.x509.pem protect-patched-unsigned-aligned.apk
+chmod +x tools/zipalign
+tools/zipalign -p 4 protect-patched-unsigned.apk protect-patched-unsigned-aligned.apk
+java -Xmx1024M -jar tools/apksigner.jar sign --key tools/testkey.pk8 --cert tools/testkey.x509.pem protect-patched-unsigned-aligned.apk
 rm protect-patched-unsigned.apk
 mv protect-patched-unsigned-aligned.apk protect-patched.apk
--- tools/download.sh
+++ tools/download.sh
@@ -5,3 +5,14 @@ wget -O apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_
 wget -O sign.jar https://github.com/appium-boneyard/sign/releases/download/1.0/sign-1.0.jar
 wget https://github.com/appium-boneyard/sign/raw/master/testkey.pk8
 wget https://github.com/appium-boneyard/sign/raw/master/testkey.x509.pem
+
+# link for macos: https://dl.google.com/android/repository/build-tools_r27.0.3-macosx.zip
+# Link for windows: https://dl.google.com/android/repository/build-tools_r27.0.3-windows.zip
+# Link for linux: https://dl.google.com/android/repository/build-tools_r27.0.3-linux.zip
+
+wget -O build-tools.zip "https://dl.google.com/android/repository/build-tools_r27.0.3-linux.zip"
+unzip build-tools.zip
+cp android-*/lib/apksigner.jar ./
+cp android-*/zipalign ./
+rm -rf android-*
+rm -f build-tools.zip
